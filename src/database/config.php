f65be60a486196d61c039deae2ee2514ecde76fa
Guilherme Castelo <guilhermecastelo.mail@gmail.com>
2023-06-07 22:07:59 -0400

    add config bd

---
+++ src/database/config.php
@@ -0,0 +1,15 @@
+<?php
+define("HOST", '127.0.0.1');
+define("USER", 'root');
+define("PASS", '');
+define("BASE", 'crud');
+
+$conn = new Mysqli(HOST, USER, PASS, BASE);
+
+// Verifica se a conexão foi estabelecida com sucesso
+if ($conn->connect_error) {
+    die("Erro de conexão: " . $conn->connect_error);
+}
+
+//echo "Conexão com o banco de dados estabelecida com sucesso!";
+?>
