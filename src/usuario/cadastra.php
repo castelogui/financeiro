5cae514101003c7231df29e8a0ed19244eff6ad5
Guilherme Castelo <guilhermecastelo.mail@gmail.com>
2023-06-07 22:08:17 -0400

    add page form cadastro

---
+++ src/usuario/cadastra.php
@@ -0,0 +1,35 @@
+<h1>Cadastrar Usuario</h1>
+<form action="?page=salvarUsuario" method="POST">
+  <input type="hidden" name="acao" value="cadastrar">
+  <div class="mb-3">
+    <label for="nome">Nome</label>
+    <input type="text" name="nome" class="form-control">
+  </div>
+  <div class="mb-3">
+    <label for="sobrenome">Sobreome</label>
+    <input type="text" name="sobrenome" class="form-control">
+  </div>
+  <div class="mb-3">
+    <label for="dtNasc">Data de Nascimento</label>
+    <input type="date" name="dtNasc" class="form-control">
+  </div>
+  <div class="mb-3">
+    <label for="username">Username</label>
+    <input type="text" name="username" class="form-control">
+  </div>
+  <div class="mb-3">
+    <label for="email">Email</label>
+    <input type="email" name="email" class="form-control">
+  </div>
+  <div class="mb-3">
+    <label for="senha">Senha</label>
+    <input type="password" name="senha" class="form-control" required>
+  </div>
+  <div class="container ">
+    <div class="row justify-content-md-center">
+      <div class="col col-lg-2">
+        <button type="submit" class="btn btn-success">Cadastrar</button>
+      </div>
+    </div>
+  </div>
+</form>
