95ea256d0c4580adf7f3f1e1d8bfeb32f852c241
Guilherme Castelo <guilhermecastelo.mail@gmail.com>
2023-06-10 01:22:33 -0400

    add page login

---
+++ src/session/index.php
@@ -0,0 +1,45 @@
+<div class="container h-100 d-flex px-0 px-sm-4">
+  <div class="row justify-content-center align-items-center m-auto">
+    <div class="col-12">
+      <div class="bg-mode shadow rounded-3 overflow-hidden">
+        <div class="row g-0">
+          <div class="col-lg-6 d-flex align-items-center order-2 order-lg-1">
+            <div class="p-2 p-lg-4">
+              <img src="assets/images/element/signin.svg" width="400px" alt="">
+            </div>
+            <div class="vr opacity-1 d-none d-lg-block"></div>
+          </div>
+
+          <div class="col-lg-6 order-1">
+            <div class="p-4 p-sm-7">
+              <a href="index.html">
+                <img class="h-50px mb-4" width="60px" src="assets/images/logo-icon.svg" alt="logo">
+              </a>
+              <h1 class="mb-2 h3">Bem vindo de volta</h1>
+              <p class="mb-0">Novo aqui?<a href="sign-up.html"> Solicite um acesso</a></p>
+
+              <form action="?page=login" method="POST" class="mt-4 text-start">
+                <div class="mb-3">
+                  <label class="form-label">Usuario</label>
+                  <input type="text" name="username" class="form-control">
+                </div>
+                <div class="mb-3 position-relative">
+                  <label class="form-label">Senha</label>
+                  <input class="form-control" name="senha" type="password">
+                </div>
+                <div class="mb-3 d-sm-flex justify-content-between">
+                  <br>
+                  <a href="forgot-password.html">Esqueceu a senha?</a>
+                </div>
+                <div>
+                  <button type="submit" class="btn btn-primary btn-block">Login</button>
+                  <button class="btn btn-light btn-block">Cancelar</button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
