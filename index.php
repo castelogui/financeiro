34c24350e670aef6bff8a8f5e49b963c3bac2e28
Guilherme Castelo <guilhermecastelo.mail@gmail.com>
2023-06-15 21:02:41 -0400

    update bootstrap and add new inks

--- index.php
+++ index.php
@@ -5,13 +5,14 @@ session_start();
 <html lang="pt-br">
 
 <head>
-  <!-- Meta tags Obrigatórias -->
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-  <!-- Bootstrap CSS -->
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+  <script src="https://kit.fontawesome.com/da1d70c44d.js" crossorigin="anonymous"></script>
   <link rel="stylesheet" href="./src/util/style-btn-switch.css">
+  <link rel="stylesheet" href="./src/util/geral.css">
   <title>Cadastro de Usuário</title>
 </head>
 
@@ -31,7 +32,7 @@ session_start();
                   <a class="nav-link" href="?page=listarUsuario">Usuários</a>
                 </li>
                 <li class="nav-item">
-                  <a class="nav-link" href="#">Contas</a>
+                  <a class="nav-link" href="?page=listarConta">Contas</a>
                 </li>
                 <li class="nav-item">
                   <a class="nav-link" href="#">Receitas</a>
@@ -74,7 +75,7 @@ session_start();
       </div>
     </div>
   </nav>
-  <div class="container">
+  <div class="container conteudo">
     <div class="row">
       <div class="col mt-5">
         <?php
@@ -101,6 +102,15 @@ session_start();
           case "logout":
             include("./src/session/logout.php");
             break;
+          case "listarConta":
+            include("./src/conta/lista.php");
+            break;
+          case "listarCategoria":
+            include("./src/conta/listaCategoria.php");
+            break;
+          case "salvarConta":
+            include("./src/conta/salvar.php");
+            break;
           default:
             include("./src/dashboard/home.php");
         }
@@ -108,11 +118,14 @@ session_start();
       </div>
     </div>
   </div>
-  <!-- JavaScript (Opcional) -->
-  <!-- jQuery primeiro, depois Popper.js, depois Bootstrap JS -->
+  <div class="myfooter">
+
+  </div>
   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js" integrity="sha384-fbbOQedDUMZZ5KreZpsbe1LCZPVmfTnH7ois6mU1QK+m14rQ1l2bGBq41eYeM/fS" crossorigin="anonymous"></script>
 
 </body>
 
