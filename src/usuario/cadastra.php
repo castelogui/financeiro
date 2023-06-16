f71507f7599fcc8b5324499ee8f255606c338fef
Guilherme Castelo <guilhermecastelo.mail@gmail.com>
2023-06-15 21:02:00 -0400

    upgrade bootstrap

--- src/usuario/cadastra.php
+++ src/usuario/cadastra.php
@@ -1,43 +1,52 @@
-<div class="row">
-  <div class="col-10">
-    <h1>Cadastrar Usuario</h1>
-  </div>
-  <div class="col-2">
-    <a class="btn btn-primary btn-lg btn-block" href="?page=listarUsuario">Voltar</a>
-  </div>
-</div>
-<br>
-<form action="?page=salvarUsuario" method="POST">
-  <input type="hidden" name="acao" value="cadastrar">
-  <div class="mb-3">
-    <label for="nome">Nome</label>
-    <input type="text" name="nome" class="form-control">
-  </div>
-  <div class="mb-3">
-    <label for="sobrenome">Sobreome</label>
-    <input type="text" name="sobrenome" class="form-control">
-  </div>
-  <div class="mb-3">
-    <label for="dtNasc">Data de Nascimento</label>
-    <input type="date" name="dtNasc" class="form-control">
-  </div>
-  <div class="mb-3">
-    <label for="username">Username</label>
-    <input type="text" name="username" class="form-control">
-  </div>
-  <div class="mb-3">
-    <label for="email">Email</label>
-    <input type="email" name="email" class="form-control">
-  </div>
-  <div class="mb-3">
-    <label for="senha">Senha</label>
-    <input type="password" name="senha" class="form-control" required>
-  </div>
-  <div class="container ">
-    <div class="row justify-content-md-center">
-      <div class="col col-lg-2">
+<?php
+$botaoCadastraUsuario = '
+<button type="button" class="btn btn-primary btn-lg btn-block" data-toggle="modal" data-target="#ExemploModalCentralizado">
+  Novo
+</button>';
+
+$modalCadastraUsuario = '
+<div class="modal fade" id="ExemploModalCentralizado" tabindex="-1" role="dialog" aria-labelledby="TituloModalCentralizado" aria-hidden="true">
+  <div class="container modal-dialog modal-dialog-centered" role="document">
+    <form class="modal-content" action="?page=salvarUsuario" method="POST">
+      <div class="modal-header">
+        <h2 class="modal-title" id="TituloModalCentralizado">
+          Cadastrar Usuario 
+        </h2>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Fechar">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <input type="hidden" name="acao" value="cadastrar">
+        <div class="mb-3">
+          <label for="nome">Nome</label>
+          <input type="text" name="nome" class="form-control">
+        </div>
+        <div class="mb-3">
+          <label for="sobrenome">Sobreome</label>
+          <input type="text" name="sobrenome" class="form-control">
+        </div>
+        <div class="mb-3">
+          <label for="dtNasc">Data de Nascimento</label>
+          <input type="date" name="dtNasc" class="form-control">
+        </div>
+        <div class="mb-3">
+          <label for="username">Username</label>
+          <input type="text" name="username" class="form-control">
+        </div>
+        <div class="mb-3">
+          <label for="email">Email</label>
+          <input type="email" name="email" class="form-control">
+        </div>
+        <div class="mb-3">
+          <label for="senha">Senha</label>
+          <input type="password" name="senha" class="form-control" required>
+        </div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
         <button type="submit" class="btn btn-success">Cadastrar</button>
       </div>
-    </div>
+    </form>
   </div>
-</form>
+</div>';
