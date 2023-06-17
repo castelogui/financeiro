ad8309ca9444951be30198b9fbed6eeb054ea82b
Guilherme Castelo <guilhermecastelo.mail@gmail.com>
2023-06-17 18:37:50 -0400

    update form edit user

--- src/usuario/edita.php
+++ src/usuario/edita.php
@@ -15,42 +15,40 @@ $row = $res->fetch_object();
 <form action="?page=salvarUsuario" method="POST">
   <input type="hidden" name="acao" value="editar">
   <input type="hidden" name="idUsuario" value="<?php print $row->idUsuario; ?>">
-  <div class="mb-3">
-    <label for="nome">Nome</label>
-    <input type="text" name="nome" value="<?php print $row->nomeUsuario ?>" class="form-control">
+  <div class="row">
+    <div class="col-md-6 mb-3">
+      <label for="nome">Nome</label>
+      <input type="text" name="nome" value="<?php print $row->nomeUsuario ?>" class="form-control">
+    </div>
+    <div class="col-md-6 mb-3">
+      <label for="sobrenome">Sobreome</label>
+      <input type="text" name="sobrenome" value="<?php print $row->sobrenomeUsuario ?>" class="form-control">
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-md-6 mb-3">
+      <label for="username">Username</label>
+      <input type="text" name="username" value="<?php print $row->username ?>" class="form-control">
+    </div>
+    <div class="col-md-6 mb-3">
+      <label for="dtNasc">Data de Nascimento</label>
+      <input type="date" name="dtNasc" value="<?php print $row->dtNascUsuario ?>" class="form-control">
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-md-6 mb-3">
+      <label for="email">Email</label>
+      <input type="email" name="email" value="<?php print $row->emailUsuario ?>" class="form-control">
+    </div>
+    <div class="col-md-6 mb-3">
+      <label for="senha">Senha</label>
+      <input type="password" name="senha" class="form-control" required>
+    </div>
   </div>
   <div class="mb-3">
-    <label for="sobrenome">Sobreome</label>
-    <input type="text" name="sobrenome" value="<?php print $row->sobrenomeUsuario ?>" class="form-control">
-  </div>
-  <div class="mb-3">
-    <label for="dtNasc">Data de Nascimento</label>
-    <input type="date" name="dtNasc" value="<?php print $row->dtNascUsuario ?>" class="form-control">
-  </div>
-  <div class="mb-3">
-    <label for="username">Username</label>
-    <input type="text" name="username" value="<?php print $row->username ?>" class="form-control">
-  </div>
-  <div class="mb-3">
-    <label for="email">Email</label>
-    <input type="email" name="email" value="<?php print $row->emailUsuario ?>" class="form-control">
-  </div>
-  <div class="mb-3">
-    <label for="senha">Senha</label>
-    <input type="password" name="senha" class="form-control" required>
-  </div>
-  <div class="mb-3">
-    <label for="senha">Status</label>
-    <div class="btn-group btn-group-toggle" data-toggle="buttons">
-      <?php
-      $status = $row->statusUsuario;
-      $funcao = $status == 0 ? 'ativar' : 'inativar';
-      print "<div class='switch'>
-            <input type='checkbox' id='toggle' name='status' class='switch-checkbox'";
-      $status == 1 ? print "checked>" : print ">";
-      print "<label onclick=\"location.href='?page=salvarUsuario&acao={$funcao}&idUsuario={$row->idUsuario}';\"
-      for='toggle' class='switch-label'></label>";
-      ?>
+    <div class="form-check form-check-reverse form-switch text-center">
+      <label for="senha">Status</label>
+      <input class="form-check-input btn btn-danger btn-block" name="status" type="checkbox" role="switch" id="flexSwitchCheck">
     </div>
   </div>
   <div class="container ">
