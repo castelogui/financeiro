26acd10976108537e5e5c73aa9556a12ae52f7ce
Guilherme Castelo <guilhermecastelo.mail@gmail.com>
2023-06-17 18:38:51 -0400

    fix function cadastrarConta

--- src/conta/salvar.php
+++ src/conta/salvar.php
@@ -1,17 +1,19 @@
 <?php
 switch ($_REQUEST["acao"]) {
   case "cadastrarConta":
-    $CategoriaConta_idCategoriaConta = $_POST["categoria"];
-    $Usuario_idUsuario = $_POST["dono"];
+    $CategoriaConta_idCategoriaConta = $_POST["idCategoriaConta"];
+    $Usuario_idUsuario = $_POST["idUsuario"];
     $nomeConta = $_POST["nomeConta"];
     $corConta = $_POST["cor"];
-    $saldoConta = $_POST["saldo"];
+    $saldoConta = $_POST["saldoConta"];
 
-    $sql = "INSERT INTO conta (CategoriaConta_idCategoriaConta, Usuario_idUsuario, nomeConta, corConta, saldoConta) VALUES ('{$CategoriaConta_idCategoriaConta}', '{$Usuario_idUsuario}', '{$nomeConta}', '{$corConta}', '{$saldoConta}')";
+   $sql = "INSERT INTO conta (CategoriaConta_idCategoriaConta, Usuario_idUsuario, nomeConta, corConta, saldoConta) 
+   VALUES ('{$CategoriaConta_idCategoriaConta}', '{$Usuario_idUsuario}', '{$nomeConta}', '{$corConta}', '{$saldoConta}')";
 
     $res = $conn->query($sql) or die($conn->error);
+
     if ($res == true) {
-      print "<script>alert('Cadastrado com sucesso!');</script>";
+      //print "<script>alert('Cadastrado com sucesso!');</script>";
       print "<script>location.href='?page=listarConta';</script>";
     } else {
       print "<script>alert('Não foi possível cadastrar!');</script>";
