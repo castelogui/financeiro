2fcfce71cb19dbfd67b32ab4d0d6d5794d98174f
Guilherme Castelo <guilhermecastelo.mail@gmail.com>
2023-06-17 18:38:04 -0400

    update form cadastro

--- src/usuario/cadastra.php
+++ src/usuario/cadastra.php
@@ -18,21 +18,25 @@ $modalCadastraUsuario = '
       </div>
       <div class="modal-body">
         <input type="hidden" name="acao" value="cadastrar">
-        <div class="mb-3">
-          <label for="nome">Nome</label>
-          <input type="text" name="nome" class="form-control">
+        <div class="row">
+          <div class="col-md-6 mb-3">
+            <label for="nome">Nome</label>
+            <input type="text" name="nome" class="form-control">
+          </div>
+          <div class="col-md-6 mb-3">
+            <label for="sobrenome">Sobreome</label>
+            <input type="text" name="sobrenome" class="form-control">
+          </div>
         </div>
-        <div class="mb-3">
-          <label for="sobrenome">Sobreome</label>
-          <input type="text" name="sobrenome" class="form-control">
-        </div>
-        <div class="mb-3">
-          <label for="dtNasc">Data de Nascimento</label>
-          <input type="date" name="dtNasc" class="form-control">
-        </div>
-        <div class="mb-3">
-          <label for="username">Username</label>
-          <input type="text" name="username" class="form-control">
+        <div class="row">
+          <div class="col-md-7 mb-3">
+            <label for="username">Username</label>
+            <input type="text" name="username" class="form-control">
+          </div>
+          <div class="col-md-5">
+            <label for="dtNasc">Data de Nascimento</label>
+            <input type="date" name="dtNasc" class="form-control">
+          </div>
         </div>
         <div class="mb-3">
           <label for="email">Email</label>
