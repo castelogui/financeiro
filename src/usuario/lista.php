4acd9991ed3d028e183c97ab1aac8ef7e28a33a2
Guilherme Castelo <guilhermecastelo.mail@gmail.com>
2023-06-18 03:00:00 -0400

    alter table

--- src/usuario/lista.php
+++ src/usuario/lista.php
@@ -30,7 +30,8 @@ $qtd = $res->num_rows;
 if ($qtd > 0) {
   print "<div class='table-responsive'>
         <table class='table table-sm table-hover table-striped table-bordered'>";
-  print  "<tr class='thead-dark'>
+  print  "<thead>
+          <tr>
             <th>#</th>
             <th>Nome</th>
             <th>Data Nascimento</th>
@@ -38,38 +39,43 @@ if ($qtd > 0) {
             <th>E-mail</th>
             <th>Status</th>
             <th>Ações</th>              
-          </tr>";
+          </tr>
+          </thead>";
   while ($row = $res->fetch_object()) {
-    print  "<tr>
-              <td>{$row->idUsuario}</td>
-              <td>{$row->nomeUsuario} {$row->sobrenomeUsuario}</td>
-              <td>{$row->dtNascUsuario}</td>
-              <td>{$row->username}</td>
-              <td>{$row->emailUsuario}</td>";
-    if ($row->statusUsuario == true) {
-      print '<td>
-              <span class="badge bg-success">Ativo</span>
-            </td>';
-    } else {
-      print '<td>
-              <span class="badge bg-warning text-white">Inativo</span>
-            </td>';
-    }
-    print  "<td>
-               <button onclick=\"location.href='?page=editarUsuario&idUsuario={$row->idUsuario}';\"
-               class='btn btn-block btn-primary'><i class='fa-solid fa-pencil fa-fade'></i> Editar</button>
-             </td>                  
-           </tr>";
+    print  "<tbody>
+              <tr>
+                <td>{$row->idUsuario}</td>
+                <td>{$row->nomeUsuario} {$row->sobrenomeUsuario}</td>
+                <td>{$row->dtNascUsuario}</td>
+                <td>{$row->username}</td>
+                <td>{$row->emailUsuario}</td>";
+      if ($row->statusUsuario == true) {
+        print '<td>
+                <span class="badge bg-success">Ativo</span>
+              </td>';
+      } else {
+        print '<td>
+                <span class="badge bg-warning text-white">Inativo</span>
+              </td>';
+      }
+      print  "<td>
+                <button onclick=\"location.href='?page=editarUsuario&idUsuario={$row->idUsuario}';\"
+                class='btn btn-block btn-primary'><i class='fa-solid fa-pencil fa-fade'></i> Editar</button>
+              </td>                  
+            </tr>
+          </tbody>";
   };
-  print  "<tr class='thead-dark'>
-            <th>#</th>
-            <th>Nome</th>
-            <th>Data Nascimento</th>
-            <th>Username</th>
-            <th>E-mail</th>
-            <th>Status</th>
-            <th>Ações</th>              
-          </tr>";
+  print  "<thead>
+            <tr>
+              <th>#</th>
+              <th>Nome</th>
+              <th>Data Nascimento</th>
+              <th>Username</th>
+              <th>E-mail</th>
+              <th>Status</th>
+              <th>Ações</th>              
+            </tr>
+          </thead>";
   print "</table>
         </div>";
 } else {
