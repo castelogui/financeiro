89aa20aa40e4eec853abf81af7e7382e07bc1186
Guilherme Castelo <guilhermecastelo.mail@gmail.com>
2023-06-18 19:31:28 -0400

    modularized pages and footer

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ $theme = "<script>localStorage.getItem('theme')</script>";
 ?>
 
 <!DOCTYPE html>
-<html lang="pt-br" data-bs-theme="<?php echo $theme?>">
+<html lang="pt-br" data-bs-theme="<?php echo $theme ?>">
 
 <head>
   <meta charset="utf-8">
@@ -16,9 +16,8 @@ $theme = "<script>localStorage.getItem('theme')</script>";
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
   <script src="https://kit.fontawesome.com/da1d70c44d.js" crossorigin="anonymous"></script>
-  <link rel="stylesheet" href="./src/util/style-btn-switch.css">
-  <link rel="stylesheet" href="./src/util/geral.css">
-  <script type="text/javascript" src="./src/util/theme.js"></script>
+  <link rel="stylesheet" href="./css/style-btn-switch.css">
+  <link rel="stylesheet" href="./css/geral.css">
 </head>
 
 <body>
@@ -40,9 +39,12 @@ $theme = "<script>localStorage.getItem('theme')</script>";
             <li class="nav-item">
               <a class="nav-link" href="#">Receitas</a>
             </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Transações</a>
+            </li>
           <?php endif; ?>
         </ul>
-        <form class="form-inline my-2 my-lg-0" >
+        <form class="form-inline my-2 my-lg-0">
           <?php if (empty($_SESSION)) : ?>
             <a class="btn btn-success my-2 my-sm-0" onclick="location.href='?page=index';">Entrar</a>
           <?php else : ?>
@@ -73,40 +75,20 @@ $theme = "<script>localStorage.getItem('theme')</script>";
       <div class="col mt-5">
         <?php
         include("./src/database/config.php");
-
-        $pages = [
-          "cadastrarUsuario" => "./src/usuario/cadastra.php",
-          "listarUsuario" => "./src/usuario/lista.php",
-          "salvarUsuario" => "./src/usuario/salvar.php",
-          "editarUsuario" => "./src/usuario/edita.php",
-          "index" => "./src/session/index.php",
-          "login" => "./src/session/login.php",
-          "logout" => "./src/session/logout.php",
-          "listarConta" => "./src/conta/lista.php",
-          "listarCategoria" => "./src/conta/listaCategoria.php",
-          "salvarConta" => "./src/conta/salvar.php",
-          "editarConta" => "./src/conta/editConta.php",
-        ];
-
-        $page = @$_REQUEST["page"];
-        if (array_key_exists($page, $pages)) {
-          include($pages[$page]);
-        } else {
-          include("./src/dashboard/home.php");
-        }
+        include("./src/util/pages.php");
         ?>
       </div>
     </div>
   </div>
-  <div class="myfooter">
-
-  </div>
+  <?php
+  include("./src/util/footer.php");
+  ?>
   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js" integrity="sha384-fbbOQedDUMZZ5KreZpsbe1LCZPVmfTnH7ois6mU1QK+m14rQ1l2bGBq41eYeM/fS" crossorigin="anonymous"></script>
-  <script src="./src/util/theme.js"></script>
+  <script src="./js/theme.js"></script>
 </body>
 
 </html>
