10fb6e285675c2bf8496be93b5935ab3e98fc445
Guilherme Castelo <guilhermecastelo.mail@gmail.com>
2023-06-18 19:31:59 -0400

    create footer

---
+++ src/util/footer.php
@@ -0,0 +1,33 @@
+<footer class="py-3">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-6">
+        <h5>Controle Financeiro</h5>
+        <p>Seu texto de descrição aqui.</p>
+      </div>
+      <div class="col-lg-3">
+        <h6>Links Úteis</h6>
+        <ul class="list-unstyled">
+          <li><a href="#">Home</a></li>
+          <li><a href="#">Sobre</a></li>
+          <li><a href="#">Serviços</a></li>
+          <li><a href="#">Contato</a></li>
+        </ul>
+      </div>
+      <div class="col-lg-3">
+        <h6>Redes Sociais</h6>
+        <ul class="list-unstyled">
+          <li><a href="#"><i class="bi bi-facebook"></i></a></li>
+          <li><a href="#"><i class="bi bi-twitter"></i></a></li>
+          <li><a href="#"><i class="bi bi-instagram"></i></a></li>
+          <li><a href="#"><i class="bi bi-linkedin"></i></a></li>
+        </ul>
+      </div>
+    </div>
+    <div class="row mt-3">
+      <div class="col-lg-12">
+        <p class="text-center">&copy; 2023 Controle Financeiro. Todos os direitos reservados.</p>
+      </div>
+    </div>
+  </div>
+</footer>
