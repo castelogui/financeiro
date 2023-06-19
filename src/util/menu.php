93c412f6c2ff74915875ff7a1ef9d9892e1357ca
Guilherme Castelo <guilhermecastelo.mail@gmail.com>
2023-06-19 19:39:05 -0400

    create menu

---
+++ src/util/menu.php
@@ -0,0 +1,14 @@
+<?php if (!empty($_SESSION)) : ?>
+  <li class="nav-item">
+    <a class="nav-link" href="?page=listarUsuario">Usuários</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="?page=listarConta">Contas</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Receitas</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Transações</a>
+  </li>
+<?php endif; ?>
