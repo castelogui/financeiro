7f55cfa13da05d148ad56ec4432599144e817219
Guilherme Castelo <guilhermecastelo.mail@gmail.com>
2023-06-19 20:55:46 -0400

    update menu

--- src/util/menu.php
+++ src/util/menu.php
@@ -7,20 +7,17 @@
     </li>
     <li class="nav-item">
       <a class="nav-link" href="?page=listarConta">
-        <i class="fa-solid fa-sack-dollar me-2"></i>
-        Contas
+        <i class="fa-solid fa-sack-dollar me-2"></i>Contas
       </a>
     </li>
     <li class="nav-item">
       <a class="nav-link" href="#">
-        <i class="fa-solid fa-file-invoice-dollar me-2"></i>
-        Receitas
+        <i class="fa-solid fa-file-invoice-dollar me-2"></i>Receitas
       </a>
     </li>
     <li class="nav-item">
       <a class="nav-link" href="#">
-        <i class="fa-solid fa-dollar-sign me-2"></i>
-        Transações
+        <i class="fa-solid fa-dollar-sign me-2"></i>Transações
       </a>
     </li>
   </ul>
