1fc45dd3fc4d918e81471f8fe0054507eeaeb478
Guilherme Castelo <guilhermecastelo.mail@gmail.com>
2023-06-19 20:48:39 -0400

    add icons

--- src/util/menu.php
+++ src/util/menu.php
@@ -1,14 +1,27 @@
 <?php if (!empty($_SESSION)) : ?>
-  <li class="nav-item">
-    <a class="nav-link" href="?page=listarUsuario">Usuários</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" href="?page=listarConta">Contas</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" href="#">Receitas</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" href="#">Transações</a>
-  </li>
+  <ul class="navbar-nav mr-auto mt-2">
+    <li class="nav-item">
+      <a class="nav-link" href="?page=listarUsuario">
+        <i class="fa-solid fa-user me-2"></i>Usuários
+      </a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="?page=listarConta">
+        <i class="fa-solid fa-sack-dollar me-2"></i>
+        Contas
+      </a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">
+        <i class="fa-solid fa-file-invoice-dollar me-2"></i>
+        Receitas
+      </a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">
+        <i class="fa-solid fa-dollar-sign me-2"></i>
+        Transações
+      </a>
+    </li>
+  </ul>
 <?php endif; ?>
