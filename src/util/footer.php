8b6b0861c94f07f842bcdc25a32695b9f68b4388
Guilherme Castelo <guilhermecastelo.mail@gmail.com>
2023-06-19 23:24:56 -0400

    modify margens of the footer

--- src/util/footer.php
+++ src/util/footer.php
@@ -1,28 +1,30 @@
-<footer class="py-3 bg-body-tertiary">
-  <div class="container mt-4">
-    <div class="row">
-      <div class="col-lg-6">
-        <h5>Controle Financeiro</h5>
-        <p class="col-lg-6">Este sistema está sendo desenvolvido para proporcionar um controle financeiro eficiente e manter suas finanças sempre em ordem.</p>
+<div class="py-3 bg-body-tertiary mt-5">
+  <footer>
+    <div class="container mt-5">
+      <div class="row">
+        <div class="col-lg-6">
+          <h5>Controle Financeiro</h5>
+          <p class="col-lg-6">Este sistema está sendo desenvolvido para proporcionar um controle financeiro eficiente e manter suas finanças sempre em ordem.</p>
+        </div>
+        <div class="col-lg-3">
+          <h6>Links Úteis</h6>
+          <?php include("./src/util/menu.php"); ?>
+        </div>
+        <div class="col-lg-3">
+          <h6>Redes Sociais</h6>
+          <ul class="list-unstyled">
+            <li><a href="#"><i class="bi bi-facebook"></i></a></li>
+            <li><a href="#"><i class="bi bi-twitter"></i></a></li>
+            <li><a href="#"><i class="bi bi-instagram"></i></a></li>
+            <li><a href="#"><i class="bi bi-linkedin"></i></a></li>
+          </ul>
+        </div>
       </div>
-      <div class="col-lg-3">
-        <h6>Links Úteis</h6>
-        <?php include("./src/util/menu.php"); ?>
-      </div>
-      <div class="col-lg-3">
-        <h6>Redes Sociais</h6>
-        <ul class="list-unstyled">
-          <li><a href="#"><i class="bi bi-facebook"></i></a></li>
-          <li><a href="#"><i class="bi bi-twitter"></i></a></li>
-          <li><a href="#"><i class="bi bi-instagram"></i></a></li>
-          <li><a href="#"><i class="bi bi-linkedin"></i></a></li>
-        </ul>
+      <div class="row mt-5">
+        <div class="col-lg-12">
+          <p class="text-center">&copy; 2023 Controle Financeiro. Todos os direitos reservados.</p>
+        </div>
       </div>
     </div>
-    <div class="row mt-3">
-      <div class="col-lg-12">
-        <p class="text-center">&copy; 2023 Controle Financeiro. Todos os direitos reservados.</p>
-      </div>
-    </div>
-  </div>
-</footer>
+  </footer>
+</div>
