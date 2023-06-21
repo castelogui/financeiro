3e5acfca7393e3ce09ceafbf7c2aa8b863d61a25
Guilherme Castelo <guilhermecastelo.mail@gmail.com>
2023-06-21 00:31:17 -0400

    create case editarConta

--- src/conta/salvar.php
+++ src/conta/salvar.php
@@ -7,7 +7,7 @@ switch ($_REQUEST["acao"]) {
     $corConta = $_POST["cor"];
     $saldoConta = $_POST["saldoConta"];
 
-   $sql = "INSERT INTO conta (CategoriaConta_idCategoriaConta, Usuario_idUsuario, nomeConta, corConta, saldoConta, statusConta) 
+    $sql = "INSERT INTO conta (CategoriaConta_idCategoriaConta, Usuario_idUsuario, nomeConta, corConta, saldoConta, statusConta) 
    VALUES ('{$CategoriaConta_idCategoriaConta}', '{$Usuario_idUsuario}', '{$nomeConta}', '{$corConta}', '{$saldoConta}', 1)";
 
     $res = $conn->query($sql) or die($conn->error);
@@ -22,7 +22,33 @@ switch ($_REQUEST["acao"]) {
     break;
 
   case "editarConta":
+    $CategoriaConta_idCategoriaConta = $_POST["CategoriaConta_idCategoriaConta"];
+    $Usuario_idUsuario = $_POST["Usuario_idUsuario"];
+    $nomeConta = $_POST["nomeConta"];
+    $corConta = $_POST["corConta"];
+    $saldoConta = $_POST["saldoConta"];
+    $statusConta = $_POST["statusConta"] == "on" ? 1 : 0;
 
+    $idcontaedit = $_REQUEST["idConta"];
+
+    $sql = "UPDATE conta 
+            SET CategoriaConta_idCategoriaConta = '{$CategoriaConta_idCategoriaConta}', 
+                Usuario_idUsuario = '{$Usuario_idUsuario}', 
+                nomeConta = '{$nomeConta}', 
+                corConta = '{$corConta}', 
+                saldoConta = '{$saldoConta}', 
+                statusConta = '{$statusConta}' 
+            WHERE idConta = " . $_REQUEST["idConta"];
+
+    $res = $conn->query($sql) or die($conn->error);
+
+    if ($res == true) {
+      //print "<script>alert('Editado com sucesso!');</script>";
+      print "<script>location.href='?page=detalhesConta&idConta={$idcontaedit}';</script>";
+    } else {
+      print "<script>alert('Não foi possível editar!');</script>";
+      print "<script>\location.href='?page=detalhesConta&idConta={$idcontaedit}';</script>";
+    }
     break;
   case "excluirConta":
 
