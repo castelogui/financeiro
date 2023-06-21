faf7b704869a97414a4ed4f9d229c7e3deddc86a
Guilherme Castelo <guilhermecastelo.mail@gmail.com>
2023-06-21 00:32:57 -0400

    fix category and users list bug

--- src/conta/lista.php
+++ src/conta/lista.php
@@ -23,9 +23,7 @@
 <br>
 
 <?php
-include('src/conta/sqls.php');
-// seleciona o tema atual da session
-$theme = "<script>localStorage.getItem('theme')</script>";
+include('src/database/querys.php');
 ?>
 
 <style>
@@ -43,23 +41,27 @@ $theme = "<script>localStorage.getItem('theme')</script>";
       $cor = $row->corConta;
 
       $saldoFormated = number_format($row->saldoConta, 2, ',', '.');
-
-      while ($rowUC = $resUsuario->fetch_object()) {
-        $rowUC->idUsuario == $row->Usuario_idUsuario ? $userConta = $rowUC->username : null;
-      }
     ?>
       <div class="col card-group">
-        <div class="card btn btn-lg alert alert-<?php echo $cor ?>" data-toggle="modal" data-target="#modalDetalhesConta">
+        <div class="card btn btn-lg alert alert-<?php echo $cor ?>" onclick="location.href='?page=detalhesConta&idConta=<?php echo $row->idConta ?>'">
           <div class="card-body">
             <h4 class="card-title">
               <?php echo $row->nomeConta ?>
             </h4>
             <div class="badge bg-<?php echo $cor ?>">
-              categoria
+              <?php
+              $categoriaContaAtual = getCategoriaContaById($row->CategoriaConta_idCategoriaConta, $conn);
+              print $categoriaContaAtual->nomeCategoriaConta
+              ?>
             </div>
             <div class="row">
               <div class="col">
-                <p class="col card-text"><?php echo "@" . $userConta ?></p>
+                <p class="col card-text">
+                  <?php
+                  $usuarioContaAtual = getUserById($row->Usuario_idUsuario, $conn);
+                  print "@" . $usuarioContaAtual->username
+                  ?>
+                </p>
               </div>
               <div class="col">
                 <p class="col card-text"><?php echo "R$" . $saldoFormated ?></p>
@@ -81,224 +83,6 @@ $theme = "<script>localStorage.getItem('theme')</script>";
   </div>
 <?php endif; ?>
 
-<div class="modal fade" id="modalCadastraConta" tabindex="-1" role="dialog" aria-labelledby="TituloModalCadastraConta" aria-hidden="true">
-  <div class="modal-dialog modal-dialog-centered" role="document">
-    <form class="modal-content" action="?page=salvarConta" method="POST">
-      <div class="modal-header">
-        <h2 class="modal-title" id="TituloModalCadastraConta">Cadastrar Conta</h2>
-        <button type="button" class="btn btn-sm btn-danger" data-dismiss="modal" aria-label="Fechar">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-        <input type="hidden" name="acao" value="cadastrarConta">
-        <div class="mb-3">
-          <label for="nomeConta">Nome da Conta</label>
-          <input type="text" name="nomeConta" class="form-control">
-        </div>
-        <div class="mb-3">
-          <label for="categoria">Categoria Conta</label>
-          <div class="input-group mb-3">
-            <select name="idCategoriaConta" class="form-select">
-              <option selected>---</option>
-              <?php
-              if ($qtdCategoria > 0) :
-                while ($rowCategoria = $resCategoria->fetch_object()) {
-                  $icone = $rowCategoria->iconeCategoriaConta;
-                  $nome = $rowCategoria->nomeCategoriaConta;
-              ?>
-                  <option value="<?php echo $rowCategoria->idCategoriaConta ?>">
-                    <i class="<?php $rowCategoria->iconeCategoriaConta ?>"></i>
-                    <?php echo $nome ?>
-                  </option>
-                <?php }
-              else : ?>
-                <option>Nenhuma Categoria Cadastrada</option>
-
-              <?php endif; ?>
-            </select>
-
-            <button class="btn btn-primary" type="button" onclick="location.href='?page=listarCategoria'">New</button>
-          </div>
-        </div>
-        <div class="mb-3">
-          <label for="usuario">Usuario</label>
-          <div class="input-group mb-3">
-            <select name="idUsuario" class="form-select">
-              <?php
-              $resUsuario = $conn->query($sqlUsuario);
-              $qtdUsuario = $resUsuario->num_rows;
-              print '<option selected>---</option>';
-              if ($qtdUsuario > 0) {
-                while ($rowUsuario = $resUsuario->fetch_object()) {
-                  $value = $rowUsuario->idUsuario;
-                  $nome = $rowUsuario->nomeUsuario . ' ' . $rowUsuario->sobrenomeUsuario;
-                  $id = $rowUsuario->idUsuario;
-                  echo <<<HTML
-                    <option value="{$value}">
-                     #{$id} {$nome}
-                    </option>
-                  HTML;
-                }
-              } else {
-                print '<option>Nenhuma Usuario Cadastrado</option>';
-              }
-              ?>
-            </select>
-            <button data-toggle="modal" data-target="#modalCadastrarCategoria" class="btn btn-primary" type="button">New</button>
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-md-6 mb-3">
-            <label for="cor">Cor</label>
-            <select name="cor" type="color" class="form-control" onchange="updateSelectClass(this)">
-              <option value="-">---</option>
-              <option value="primary" class="badge text-bg-primary">Primary</option>
-              <option value="secondary" class="badge text-bg-secondary">Secondary</option>
-              <option value="success" class="badge text-bg-success">Success</option>
-              <option value="danger" class="badge text-bg-danger">Danger</option>
-              <option value="warning" class="badge text-bg-warning">Warning</option>
-              <option value="info" class="badge text-bg-info">Info</option>
-              <option value="light" class="badge text-bg-light">Light</option>
-              <option value="dark" class="badge text-bg-dark">Dark</option>
-            </select>
-          </div>
-          <div class="col-md-6 mb-3">
-            <label for="saldo">Saldo Inicial</label>
-            <div class="input-group mb-3">
-              <span class="input-group-text">R$</span>
-              <input type="text" name="saldoConta" class="form-control" aria-label="Amount (to the nearest dollar)">
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
-        <button type="submit" class="btn btn-success">Cadastrar</button>
-      </div>
-    </form>
-  </div>
-</div>
-
-
-<div class="modal fade" id="modalDetalhesConta" tabindex="-1" role="dialog" aria-labelledby="TituloModalEditaConta" aria-hidden="true">
-  <div class="modal-dialog modal-dialog-centered" role="document">
-    <form class="modal-content" action="?page=salvarConta" method="POST">
-      <div class="modal-header">
-        <div class="mb-3">
-          <h2 class="modal-title" id="TituloModalEditaConta">Detalhes Conta </h2>
-          <label for="usuario">
-            Usuario: <span class="badge bg-primary">Nome do Usuario</span>
-          </label>
-        </div>
-        <div class="btn-toolbar" role="toolbar">
-          <div class="btn-group m-2" role="group">
-            <button onclick="location.href='?page=editarConta&idConta='" type="button" class="btn btn-sm btn-primary" data-dismiss="modal" aria-label="Fechar">
-              <span aria-hidden="true"><i class="fa-solid fa-pen-to-square"></i></span>
-            </button>
-          </div>
-          <div class="btn-group m-2" role="group">
-            <button type="button" class="btn btn-sm btn-danger" data-dismiss="modal" aria-label="Fechar">
-              <span aria-hidden="true"><i class="fa-solid fa-xmark"></i></span>
-            </button>
-          </div>
-        </div>
-      </div>
-      <div class="modal-body">
-        <input type="hidden" name="acao" value="cadastrarConta">
-        <div class="row">
-          <div class="col-md-6 mb-3">
-            <label for="nomeConta">
-              Conta: <span class="badge bg-primary">Nome</span>
-            </label>
-          </div>
-          <div class="col-md-6 mb-3">
-            <label for="categoria">
-              Categoria: <span class="badge bg-primary">Categoria</span>
-            </label>
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-md-6 mb-3">
-            <label for="cor">
-              Cor: <span class="badge bg-primary">Cor</span>
-            </label>
-          </div>
-          <div class="col-md-6 mb-3">
-            <label for="saldo">
-              Saldo: <span class="badge bg-primary">R$ 0,00</span>
-            </label>
-          </div>
-        </div>
-        <table class="table">
-          <form class="form-inline my-2 my-lg-0">
-            <div class="row">
-              <h4>Extrato</h4>
-              <div class="input-group mb-3">
-                <select class="form-select text-bg-primary">
-                  <option selected>Filtrar</option>
-                  <option value="1">One</option>
-                  <option value="2">Two</option>
-                  <option value="3">Three</option>
-                </select>
-                <input type="date" class="form-control" name="" id="">
-                <span class="input-group-text">-</span>
-                <input type="date" class="form-control" name="" id="">
-                <button class="btn btn-primary" type="button" id="button-addon2">
-                  <i class="fa-solid fa-magnifying-glass"></i>
-                </button>
-              </div>
-            </div>
-          </form>
-          <thead>
-            <tr>
-              <th scope="col">#</th>
-              <th scope="col">Categoria</th>
-              <th scope="col">Data Hora</th>
-              <th scope="col">Valor</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <th scope="row">8</th>
-              <td>Mercado</td>
-              <td>02 Jun 23 - 14:59</td>
-              <td>R$ 23,80</td>
-            </tr>
-            <tr>
-              <th scope="row">7</th>
-              <td>Padaria</td>
-              <td>01 Jun 23 - 07:33</td>
-              <td>R$ 13,46</td>
-            </tr>
-            <tr>
-              <th scope="row">6</th>
-              <td>Luz</td>
-              <td>01 Jun 23 - 19:41</td>
-              <td>R$ 143</td>
-            </tr>
-          </tbody>
-          <tfooter>
-            <tr>
-              <th scope="col">#</th>
-              <th scope="col">Categoria</th>
-              <th scope="col">Data Hora</th>
-              <th scope="col">R$ 320.43</th>
-            </tr>
-          </tfooter>
-        </table>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
-        <button type="submit" class="btn btn-success">Cadastrar</button>
-      </div>
-    </form>
-  </div>
-</div>
-
-<script>
-  function updateSelectClass(select) {
-    var selectedColor = select.value;
-    select.className = 'form-control badge ' + 'text-bg-' + selectedColor;
-  }
-</script>
+<?php
+include('src/conta/modalCadastraConta.php');
+?>
