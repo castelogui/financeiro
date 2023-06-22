644a5b86f61aa20944e5d3f5e7a1beaf0e3d5b4f
Guilherme Castelo <guilhermecastelo.mail@gmail.com>
2023-06-22 00:03:50 -0400

    alter table usuarios for card element

--- src/usuario/lista.php
+++ src/usuario/lista.php
@@ -23,67 +23,64 @@
   </div>
 </div>
 <br>
+<style>
+  .card:hover {
+    transform: scale(1.04);
+    box-shadow: 0 0 15px rgba(0, 0, 0, 1);
+    transition: all 0.2s ease-in-out;
+    cursor: pointer;
+  }
+</style>
 <?php
 $sql = "SELECT * FROM usuario";
 $res = $conn->query($sql);
 $qtd = $res->num_rows;
 if ($qtd > 0) {
-  print "<div class='table-responsive'>
-        <table class='table table-sm table-hover table-striped table-bordered'>";
-  print  "<thead>
-          <tr>
-            <th>#</th>
-            <th>Nome</th>
-            <th>Data Nascimento</th>
-            <th>Username</th>
-            <th>E-mail</th>
-            <th>Status</th>
-            <th>Ações</th>              
-          </tr>
-          </thead>";
+  echo $qtd <= 3 ? "<div class='row row-cols-1 row-cols-md-$qtd g-4'>" : '<div class="row row-cols-1 row-cols-md-3 g-4">';
   while ($row = $res->fetch_object()) {
-    print  "<tbody>
-              <tr>
-                <td>{$row->idUsuario}</td>
-                <td>{$row->nomeUsuario} {$row->sobrenomeUsuario}</td>
-                <td>{$row->dtNascUsuario}</td>
-                <td>{$row->username}</td>
-                <td>{$row->emailUsuario}</td>";
-      if ($row->statusUsuario == true) {
-        print '<td>
-                <span class="badge bg-success">Ativo</span>
-              </td>';
-      } else {
-        print '<td>
-                <span class="badge bg-warning text-white">Inativo</span>
-              </td>';
-      }
-      print  "<td>
-                <button onclick=\"location.href='?page=editarUsuario&idUsuario={$row->idUsuario}';\"
-                class='btn btn-block btn-primary'><i class='fa-solid fa-pencil fa-fade'></i> Editar</button>
-              </td>                  
-            </tr>
-          </tbody>";
-  };
-  print  "<thead>
-            <tr>
-              <th>#</th>
-              <th>Nome</th>
-              <th>Data Nascimento</th>
-              <th>Username</th>
-              <th>E-mail</th>
-              <th>Status</th>
-              <th>Ações</th>              
-            </tr>
-          </thead>";
-  print "</table>
-        </div>";
-} else {
-  print "
-      <div class='card alert alert-danger'>
-        <div class='card-body'>
-          <h5 class='card-title card-danger'>Nenhum usuario Encontrado!</h5>
-        </div>
-      </div>";
-}
 ?>
+    <div class="col">
+      <div class="card h-100" onclick="location.href='?page=editarUsuario&idUsuario=<?php echo $row->idUsuario ?>'">
+        <div class="card-body">
+          <div class="row">
+            <div class="col-8">
+              <h5 class="card-title"><?php echo $row->username  ?></h5>
+            </div>
+            <div class="col-1 m-0">
+              <img src="../../assets/images/users-demo/<?php echo $row->idUsuario ?>" width="40" class="rounded mr-3" alt="" class="card-img-top">
+            </div>
+          </div>
+          <h5 class="card-title"><?php echo  $row->nomeUsuario . ' ' . $row->sobrenomeUsuario  ?></h5>
+          <div class="text-right">
+            <small class="badge badge-secondary">
+              <?php echo $row->username == 'root' ? "Administrador" : "Usuário" ?>
+            </small>
+          </div>
+          <p class="card-text"><?php echo $row->emailUsuario ?></p>
+        </div>
+        <div class="card-footer">
+          <div class="row">
+            <div class="col-6">
+              <p class="card-text">Saldo:</p>
+              <small class="badge badge-primary">0,00</small>
+            </div>
+            <div class="col-6">
+              <p class="card-text">Status:</p>
+              <small class="badge badge-<?php echo $row->statusUsuario == 1 ? "success" : "danger" ?>">
+                <?php echo $row->statusUsuario == 1 ? "Ativo" : "Inativo" ?>
+              </small>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  <?php
+  }
+  echo '</div>';
+} else { ?>
+  <div class='card alert alert-danger'>
+    <div class='card-body'>
+      <h5 class='card-title card-danger'>Nenhum usuario Encontrado!</h5>
+    </div>
+  </div>";
+<?php } ?>
