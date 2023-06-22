60998baddd6eca124fe3bb86ccfe60f75fe396a3
Guilherme Castelo <guilhermecastelo.mail@gmail.com>
2023-06-21 22:43:37 -0400

    remove cases of the categoriaConta

--- src/conta/salvar.php
+++ src/conta/salvar.php
@@ -53,29 +53,4 @@ switch ($_REQUEST["acao"]) {
   case "excluirConta":
 
     break;
-
-  case "cadastrarCategoria":
-    $nomeCategoriaConta = $_POST["nomeCategoriaConta"];
-    $iconeCategoriaConta = $_POST["iconeCategoriaConta"];
-    $statusCategoriaConta = 1;
-
-    $sql = "INSERT INTO `categoriaconta` (`idCategoriaConta`, `nomeCategoriaConta`, `iconeCategoriaConta`, `statusCategoriaConta`) 
-              VALUES (NULL, '{$nomeCategoriaConta}', '{$iconeCategoriaConta}', '{$statusCategoriaConta}')";
-
-    $res = $conn->query($sql) or die($conn->error);
-
-    if ($res == true) {
-      //print "<script>alert('Cadastrado com sucesso!');</script>";
-      print "<script>location.href='?page=listarCategoria';</script>";
-    } else {
-      print "<script>alert('Não foi possível cadastrar!');</script>";
-      print "<script>location.href='?page=listarCategoria';</script>";
-    }
-    break;
-
-  case "editarCategoria":
-
-    break;
-
-  case "excluirCategoria":
 }
