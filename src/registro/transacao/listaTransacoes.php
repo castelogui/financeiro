46c214b3cff236e61801165c4dde6b5dfe4bff5e
Guilherme Castelo <guilhermecastelo.mail@gmail.com>
2023-06-24 23:49:57 -0400

    create list transations

---
+++ src/registro/transacao/listaTransacoes.php
@@ -0,0 +1,23 @@
+<div class="row">
+  <nav aria-label="breadcrumb">
+    <ol class="breadcrumb">
+      <li class="breadcrumb-item active">
+        <a class="btn btn-sm btn-link" onclick="location.href='?page=listarTransacoes'">Transações</a>
+      </li>
+      <li class="breadcrumb-item">
+        <a class="btn btn-sm btn-link" onclick="location.href='?page=listarCategoriaRegistro'">Categorias</a>
+      </li>
+    </ol>
+  </nav>
+</div>
+<div class="row">
+  <div class="col-10">
+    <h1>Transações</h1>
+  </div>
+  <div class="col-2">
+    <button type="button" class="btn btn-primary btn-lg btn-block" data-toggle="modal" data-target="#modalCadastraRegistro">
+      <i class="fa-solid fa-plus"></i> Novo
+    </button>
+  </div>
+</div>
+<br>
