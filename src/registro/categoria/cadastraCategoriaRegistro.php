fdfffebc6f30883e4a543eecafed231207eb3edf
Guilherme Castelo <guilhermecastelo.mail@gmail.com>
2023-06-25 01:21:21 -0400

    add cadastra categoria pai

--- src/registro/categoria/cadastraCategoriaRegistro.php
+++ src/registro/categoria/cadastraCategoriaRegistro.php
@@ -54,11 +54,31 @@
         </div>
         <div class="mb-6">
           <div class="row">
-            <div class="col-md-6">
+            <!--Seleciona a categoria pai-->
+            <div class="col-md-4">
+              <label for="idCategoriaPai">Filha de:</label>
+              <select name="idCategoriaPai" class="form-select">
+                <option value="0" selected>---</option>
+                <?php
+                $categorias = getCategoriaRegistroPai($conn);
+                while ($categoria = $categorias->fetch_object()) {
+                  echo "<option value=" . $categoria->idCategoriaRegistro . ">" . $categoria->descricaoCategoriaRegistro . "</option>";
+                }
+                ?>
+              </select>
+            </div>
+            <div class="col-md-2">
+              <label for="permiteFilhos">Pai</label>
+              <div class="form-check form-switch">
+                <input class="form-check-input" name="permiteFilhos" type="checkbox" role="switch" id="permiteFilhos">
+                <label class="form-check-label" for="permiteFilhos"></label>
+              </div>
+            </div>
+            <div class="col-md-4">
               <label for="corCategoriaRegistro">Cor</label>
-              <select name="corCategoriaRegistro" type="color" class="form-control badge>
-            <option selected >---</option>
-            <option value=" primary" class="badge text-bg-primary">Primary</option>
+              <select name="corCategoriaRegistro" type="color" class="form-control badge">
+                <option selected>---</option>
+                <option value=" primary" class="badge text-bg-primary">Primary</option>
                 <option value="secondary" class="badge text-bg-secondary">Secondary</option>
                 <option value="success" class="badge text-bg-success">Success</option>
                 <option value="danger" class="badge text-bg-danger">Danger</option>
@@ -68,14 +88,7 @@
                 <option value="dark" class="badge text-bg-dark">Dark</option>
               </select>
             </div>
-            <div class="col-md-3 ">
-              <label for="permiteFilhos">Pai</label>
-              <div class="form-check form-switch">
-                <input class="form-check-input" name="permiteFilhos" type="checkbox" role="switch" id="permiteFilhos">
-                <label class="form-check-label" for="permiteFilhos"></label>
-              </div>
-            </div>
-            <div class="col-md-3">
+            <div class="col-md-2">
               <label for="statusCategoriaRegistro">Status</label>
               <div class="form-check form-switch">
                 <input class="form-check-input" type="checkbox" role="switch" name="statusCategoriaRegistro" id="statusCategoriaRegistro">
