9d30e2d4e87bcf10e7cb8803224d679c46dceef5
Guilherme Castelo <guilhermecastelo.mail@gmail.com>
2023-06-25 22:23:33 -0400

    define pass root

--- src/database/config.php
+++ src/database/config.php
@@ -1,7 +1,7 @@
 <?php
 define("HOST", '127.0.0.1');
 define("USER", 'root');
-define("PASS", '');
+define("PASS", 'root');
 define("BASE", 'financeiro');
 
 $conn = new Mysqli(HOST, USER, PASS, BASE);
